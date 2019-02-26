d59ce5941bd0081eb142de98e38ebffe3e52a7a2
Petri Kainulainen <petri.kainulainen@gmail.com>
2019-02-26 21:35:19 +0200

    Add example: Writing Selenium Tests Which Use a Dockerized Web Browser

---
+++ testproject-testcontainers-selenium/README.md
@@ -0,0 +1,8 @@
+# Writing Selenium Tests Which Use a Dockerized Web Browser
+
+This example demonstrates how we can write Selenium tests which use a Dockerized
+web browser.
+
+We can run our tests by running the command:
+
+    mvn clean test
---
+++ testproject-testcontainers-selenium/src/test/java/io/testproject/blog/testcontainers/ContainerStartedBeforeEachTestMethodTest.java
@@ -0,0 +1,45 @@
+package io.testproject.blog.testcontainers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.File;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@DisplayName("Starts the docker container before a test method is run")
+class ContainerStartedBeforeEachTestMethodTest {
+
+    @Container
+    private final BrowserWebDriverContainer BROWSER_CONTAINER = new BrowserWebDriverContainer()
+            .withCapabilities(new ChromeOptions())
+            .withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.RECORD_FAILING, new File("./target/"));
+
+    private WebDriver browser;
+
+    @BeforeEach
+    void configureBrowser() {
+        browser = BROWSER_CONTAINER.getWebDriver();
+    }
+
+    @Test
+    @DisplayName("The testproject.io web site should have the correct title")
+    void testProjectWebSiteShouldHaveCorrectTitle() {
+        browser.get("https://www.testproject.io");
+        assertThat(browser.getTitle()).isEqualTo("TestProject - Community Powered Test Automation");
+    }
+
+    @Test
+    @DisplayName("The testproject.io blog should have the correct title")
+    void testProjectBlogShouldHaveCorrectTitle() {
+        browser.get("https://blog.testproject.io/");
+        assertThat(browser.getTitle()).isEqualTo("TestProject - Test Automation Blog");
+    }
+}
